44565489c52223f726efca79ac567a6574a79058
Bèr Kessels <ber@webschuur.com>
2012-06-23 10:55:08 +0200

    HTML for now simple and ugly looking. Needs style and structure still.

--- templates/landing.php
+++ templates/landing.php
@@ -1,1 +1,37 @@
 <h2>Lid worden</h2>
+
+<p>
+
+</p>
+
+<form action="pirate" method="post" accept-charset="utf-8">
+  <div class="form-wrapper">
+    <label for="initials">Voorletters:<span class="required-marker">*</span></label><input type="text" name="initials" value="" id="initials">
+  </div>
+  <div class="form-wrapper">
+    <label for="name">Achternaam:<span class="required-marker">*</span></label><input type="text" name="name" value="" id="name">
+  </div>
+  <div class="form-wrapper">
+    <label for="email">E-mailadres:<span class="required-marker">*</span></label><input type="email" name="email" value="" id="email">
+  </div>
+  <div class="form-wrapper">
+    <label for="address">Adres:<span class="required-marker">*</span></label><textarea name="address" rows="3" cols="40"></textarea>
+  </div>
+  <div class="form-wrapper">
+    <label for="city">Woonplaats:<span class="required-marker">*</span></label><input type="text" name="city" value="" id="city">
+  </div>
+
+  <div class="row">
+    <div class="two columns big">
+      <label>Kosten:</label></br>
+      € 17.50
+    </div>
+    <div class="two columns">
+      <label>Betalen met:</label></br>
+      <img src="ideal.png" alt="iDeal logo"/>
+      <span class="ideal">iDeal</span>
+    </div>
+  <div class="form-wrapper">
+    <input type="submit" value="Opslaan en betalen">
+  </div>
+</form>
--- templates/pirate.php
+++ templates/pirate.php
@@ -1,1 +1,6 @@
 This is pirate.php
+<form action="<?php print $url ?>" method="post" accept-charset="utf-8">
+  <?php print $hidden_form ?>
+  <p><input type="submit" value="Continue →"></p>
+</form>
+
