8f3f10ff6c389f72c8fc95029528ca21024e381d
Bèr Kessels <ber@webschuur.com>
2012-06-25 10:32:45 +0200

    Add texts.
    Somewhat decent texts, instead of Lipsum.

--- templates/landing.php
+++ templates/landing.php
@@ -1,11 +1,11 @@
 <h1>Lid worden</h1>
 
 <p>
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  Mooi, je wilt lid worden! Vul onderstaand formulier in, en klik <em>Opslaan en Betalen</em>. 
+  Je komt dan bij een bevestigingspagina, waarmee je de iDeal betaling kunt afronden bij je eigen bank.<br/>
+  Wij krijgen een mail, kijken alles met de hand na en sturen je per mail een update van je lidmaatschap, dat kan dus eventjes duren.
 </p>
+
 <?php if ($flash['error']): ?>
   <div class="messages error"><?php print $flash["error"] ?></div>
 <?php endif; ?>
